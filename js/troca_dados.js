e683d7b14972194eb08a022a10820baa362a75c3
Jackson Barbosa <jbs@ic.ufal.br>
2018-04-08 19:26:25 -0300

    Redirecionamento a página do curso de ciência da computação

--- js/troca_dados.js
+++ js/troca_dados.js
@@ -1,7 +1,16 @@
-function le_curso(){
-	nome = document.getElementById('nome').innerHTML.value;
-	idade = document.getElementById("idade").innerHTML.value;
-	curso = document.getElementById("curso").innerHTML.value;
-	msg = "Dados:\n" + nome + '\n' + idade + '\n' + curso;
-	alert(msg);
+window.onload = function () {
+	nome = prompt("Digite seu nome", "");
+	idade = parseInt(prompt("Digite sua idade", ""));
+	curso = prompt("Digite seu curso", "");
+	if(curso.toUpperCase() == ("CIÊNCIA DA COMPUTAÇÃO")) {
+		window.location.href = "ciencia_computacao/ciencia_computacao.html";
+	} else {
+		alert("Curso não cadastrado!");
+		test = prompt("Digite 1 para digitar novamente e outro valor para sair");
+		if(test == '1'){
+			location.reload();
+		} else {
+			window.location.href = "index.html";
+		}
+	}
 }
